1a5e6bb61a69ba099e8a9901dcc5da60143eb5ac
Chetan Thakur <80780935+Amcelo13@users.noreply.github.com>
2023-03-05 09:56:41 +0530

    border image changes

--- components/Message.tsx
+++ components/Message.tsx
@@ -9,7 +9,7 @@ function Message({message}: Props ) {
   return (
     <div className={`py-5 text-white ${isChatGPT && "bg-[#434654]"}`}>   
         <div className="flex space-x-5 px-10 max-w-2xl mx-auto">
-            <img src={message.user.avatar} alt="" className="h-8 w-8" />
+            <img src={message.user.avatar} alt="" className="h-10 w-11 rounded-full " />
             <p className="pt-1 text-sm ">{message.text}</p>
         </div>
     </div>
