fbec7a1521043615c827a6adea13892f138c6a49
Chetan Thakur <80780935+Amcelo13@users.noreply.github.com>
2023-03-09 12:35:54 +0530

    font editing

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ function HomePage() {
   return (
     <div className="flex flex-col items-center justify-center h-screen
     px-2 text-white ">
-        <h1 className="text-5xl font-bold ">ChatGPT</h1>
+        <h1 className="text-5xl font-semibold mb-10">ChatGPT</h1>
 
         <div className='flex space-x-2 text-center '>
            <div>
